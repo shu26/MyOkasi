7498ef7dd8ffb24ae07b5a545ae7bcbf60caf801
海法修平 <fnmmbestsk15kkpo@gmail.com>
2019-08-07 22:54:40 +0900

    Initial Commit

---
+++ MyOkasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkasi
+//
+//  Created by 海法修平 on 2019/08/07.
+//  Copyright © 2019 shu26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
